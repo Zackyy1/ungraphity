102869cf11d44bfc31479c611b6e3fbd57c05569
Martin Gontsarov <38585862+Zackyy1@users.noreply.github.com>
2024-08-02 00:13:44 +0300

    Dev (#2)
    * started trackable detail view, added record submission and deletion in context menu
    
    * added charts to display all records for trackables
    
    ---------
    
    Co-authored-by: Zackyy1 <zackyydev@gmail.com>

--- src/components/trackableDetailView/trackableContextButton/newRecordForm.tsx
+++ src/components/trackableDetailView/trackableContextButton/newRecordForm.tsx
@@ -15,20 +15,25 @@ import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form";
 import { useCreateRecord } from "@/hooks/useCreateRecord";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { DatePicker } from "@/components/ui/datePicker";
 
-type CreateRecordFormData = { value: number };
+type CreateRecordFormData = { value: number; date: Date };
 
 export const NewRecordForm = ({ trackableId }: { trackableId: string }) => {
   const form = useForm();
   const addRecord = useCreateRecord(() => form.reset());
+  const [date, setDate] = React.useState<Date>(new Date());
 
   const newRecordSchema = z.object({
     value: z.number(),
+    date: z.date(),
   });
   const onSubmit: SubmitHandler<CreateRecordFormData> = (
     data: z.infer<typeof newRecordSchema>,
   ) => {
-    addRecord(trackableId, data.value);
+    console.log(data.value, date);
+    addRecord({ trackableId, value: data.value, date });
   };
 
   return (
@@ -55,6 +60,25 @@ export const NewRecordForm = ({ trackableId }: { trackableId: string }) => {
             </FormItem>
           )}
         />
+        {/* <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="block">Date</FormLabel>
+              <FormControl> */}
+        <DatePicker
+          // {...field}
+          mode="single"
+          selected={new Date()}
+          onSelect={(date) => setDate(date ?? new Date())}
+          className="w-full rounded-md border"
+        />
+        {/* </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> */}
         <DialogClose asChild>
           <Button className="w-full" type="submit" variant="default">
             Add record
