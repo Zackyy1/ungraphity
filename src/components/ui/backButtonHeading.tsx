3a4be0a66ffb61674d33c58ab41486abd1052a1d
Zackyy1 <zackyydev@gmail.com>
2024-08-07 20:22:32 +0300

    added period selector

--- src/components/ui/backButtonHeading.tsx
+++ src/components/ui/backButtonHeading.tsx
@@ -5,16 +5,21 @@ import { Heading } from "./heading";
 type BackButtonHeadingProps = {
   backButtonProps?: React.ComponentProps<typeof BackButton>;
   headingProps?: React.ComponentProps<typeof Heading>;
+  extraContent?: React.ReactNode;
 };
 
 export const BackButtonHeading = ({
   backButtonProps,
   headingProps,
+  extraContent,
 }: BackButtonHeadingProps) => {
   return (
-    <div className="items-left mb-6 flex flex-row space-x-2">
-      <BackButton {...backButtonProps} />
-      <Heading {...headingProps} className="text-2xl" />
+    <div className="w-full flex flex-col sm:flex-row sm:justify-between justify-start mb-6 gap-y-4 sm:items-center">
+      <div className="items-left flex flex-row space-x-2">
+        <BackButton {...backButtonProps} />
+        <Heading {...headingProps} className="text-2xl" />
+      </div>
+      {extraContent}
     </div>
   );
 };
